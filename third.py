518dc4507594b469e75e08c627f71f1ee7a0fb57
vlreddy123 <99265985+vlreddy123@users.noreply.github.com>
2022-02-10 18:27:17 +0530

    Create third.py

---
+++ third.py
@@ -0,0 +1,20 @@
+sno=int(input("enter the sno:"))
+sname=str(input("ente the sname:"))
+sgroup=str(input("ente the sgroup:"))
+s1=float(input("maths marks:"))
+s2=float(input("physics marks:"))
+s3=float(input("computer science marks:"))
+if s1>=35 and s2>=32 and s3>=35:
+    print("pass")
+else:
+    print("fail")
+     
+output:
+enter the sno:1
+ente the sname:vijay 
+ente the sgroup:mpcs
+maths marks:80
+physics marks:45
+computer science marks:90
+pass
+     
